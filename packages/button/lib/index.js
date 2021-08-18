207e3fbe30f92d8454949c24caaa20e0213fc488
冯振威 <1181399414@qq.com>
2021-08-18 16:53:28 +0800

    feat: 添加package.json&&tsconfig.json

---
+++ packages/button/lib/index.js
@@ -0,0 +1,4 @@
+import React from 'react';
+export const Button = () => {
+    return React.createElement(React.Fragment, null, "222");
+};
---
+++ packages/ext-react/src/index.ts
@@ -0,0 +1,7 @@
+//  export { default as Button } from "@ext-react/button";
+//  export * from "@ext-react/button";
+import React from 'react'
+
+export const a = () => {
+  return <>333</>
+}
